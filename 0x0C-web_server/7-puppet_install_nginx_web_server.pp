fc0a07f568f52d4529095079124081c2abd977fa
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-03 22:27:55 +0300

    modified script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,66 +1,20 @@
-# Puppet Manifest to install and configure Nginx web server
-
-# Install Nginx package
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
   ensure => installed,
 }
 
-# Define Nginx service
-service { 'nginx':
-  ensure => running,
-  enable => true,
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-# Configure Nginx main configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  replace => true,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Create a custom index.html file
 file { '/var/www/html/index.html':
-  ensure  => file,
   content => 'Hello World!',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
 
-# Create a custom 404.html file
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => 'Ceci n\'est pas une page',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Set up a 301 redirect for /redirect_me
-nginx::resource::vhost { 'redirect_me':
-  ensure      => present,
-  www_root    => '/var/www/html',
-  listen_port => '80',
-  proxy       => 'http://www.youtube.com',
-  redirects   => [
-    {
-      'from'   => '/redirect_me',
-      'to'     => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-      'status' => '301',
-    },
-  ],
-}
-
-# Ensure Nginx listens on port 80
-nginx::resource::vhost { 'default':
-  ensure      => present,
-  www_root    => '/var/www/html',
-  listen_port => '80',
-}
-
-# Restart Nginx if the configuration changes
 service { 'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
 }
