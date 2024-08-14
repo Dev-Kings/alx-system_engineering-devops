0159d7d894a2b6bd457c339cc6290ae8295dd96e
Dev-Kings <kingasiadavid41@gmail.com>
2024-08-14 22:53:22 +0300

    checkers made me go nuts

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,6 @@
-# This manifest fixes the 500 Internal Server Error by correcting file
-# permissions and paths for the Apache web server.
+# Fix php extensions
 
-file { '/var/www/html/index.php':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => '<?php phpinfo(); ?>',
-}
-
-exec { 'fix-apache-permissions':
-  command => 'chmod 755 /var/www/html/index.php',
-  onlyif  => 'test $(stat -c "%a" /var/www/html/index.php) != 755',
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/var/www/html/index.php'],
+exec {'fix php':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
